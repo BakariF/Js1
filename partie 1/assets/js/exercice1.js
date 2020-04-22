cf9c13077e8a5904a3300abf1174ea62d96f2d30
BakariF <bakkfofhit@live.fr>
2020-04-22 15:14:40 +0200

    first commit

---
+++ partie 1/assets/js/exercice1.js
@@ -0,0 +1,38 @@
+/* var myMessage;
+myMessage = 2;
+alert("résultat"); */
+
+/* je sépare les exercices avec des commentaires, fermer les exercices pour en faire fonctionner un autre */
+
+/* var first, second, result;
+first = prompt ('une partie de phrase :');
+second = prompt ('qui se suit');
+alert (first + second);
+ */
+
+
+
+/* je sépare les exercices avec des commentaires, fermer les exercices pour en faire fonctionner un autre */
+
+
+  /*   var name, age, city;
+
+    name = 'Jean';
+    age = 10 + 20;
+    city = 'Paris';
+
+    alert("Bonjour, je m'apelle "+name+" j'ai "+age+" et j'habite "+city) */
+
+
+/* je sépare les exercices avec des commentaires, fermer les exercices pour en faire fonctionner un autre */
+
+
+/* var first, second, third, result;
+first = prompt ('Nom');
+second = prompt ('Age');
+third = prompt ('Ville')
+result = ("Bonjour, je m'apelle "+first+" j'ai "+second+" ans et j'habite "+third)
+confirm(result); */
+
+/* je sépare les exercices avec des commentaires, fermer les exercices pour en faire fonctionner un autre */
+
